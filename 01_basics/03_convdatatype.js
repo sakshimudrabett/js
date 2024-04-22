c90ee30c9374e95c98397394918a338908c263af
Sakshi Mudrabett <144533777+sakshimudrabett@users.noreply.github.com>
2024-04-22 12:31:46 +0000

    conversion of datattype

---
+++ 01_basics/03_convdatatype.js
@@ -0,0 +1,15 @@
+let score="23"
+
+console.log(typeof score);//(typeof(score))
+
+let valueInno=Number(score)
+console.log(typeof valueInno);
+
+//"33" => 33
+//"33abc" => NAN(not a number)
+// true=>1; false=0;
+
+//1 =>true; 0=>false; 
+//" "=>false;
+//"sakshi"=>true;
+
