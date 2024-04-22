fe2ad9b81cea4d081218e1c73a78683ba41e96fe
Sakshi Mudrabett <144533777+sakshimudrabett@users.noreply.github.com>
2024-04-22 12:04:54 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict";//treat all JS code as newe version
+ //alert(3+3)//we are using nodejs not browser
+ //code readability should be high
+
+ let name="sakshi"
+ let age=20
+ let isloggedin=false
+
+ // number => 2^53
+ // string=>" " 
+ // boolean=> false/true
+ // null =>standalone value
+ // undefined => 
+ // symbol =>unique
+
+ // object
+
+ console.log(typeof age);
+
+ console.log(typeof null);//object
+ console.log(typeof undefined);//undefined
