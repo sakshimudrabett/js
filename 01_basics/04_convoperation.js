b1a8ff09665292891c54101990e3e10fff63c89b
Sakshi Mudrabett <144533777+sakshimudrabett@users.noreply.github.com>
2024-04-22 17:36:41 +0000

    coversions and operators

---
+++ 01_basics/04_convoperation.js
@@ -0,0 +1,8 @@
+//Operations
+console.log(3+3);
+
+console.log("1"+2);
+console.log(2+"1");
+console.log("1"+2+2);//if its declared as string before , everything is declared later is treated as string
+console.log(2+2+"3");//if its numer before , it adds two number and then concatenates with the string
+
