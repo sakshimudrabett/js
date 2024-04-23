f259827701b56966e5232b1769d8085ab388c333
Sakshi Mudrabett <144533777+sakshimudrabett@users.noreply.github.com>
2024-04-23 06:13:24 +0000

    Date&Time

---
+++ 01_basics/dateandtime.js
@@ -0,0 +1,19 @@
+//Dates
+
+let mydate=new Date()
+/*console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toISOString());
+console.log(mydate.toJSON());*/
+
+let mycreateddate=new Date(2024,0,25)
+let mycreateddate=new Date(2024,0,23,5,3)
+let mycreateddate=new Date("2023-01-14")
+//console.log(mycreateddate.toLocaleString());
+
+//to customise your date--->
+// newdate.tolocalestring('default',{
+//   weekday:"long",
+})
+
+//+++TIME++++++++++++
