c242107153ec4350bf78c62b4c3ec1b60060c576
Sakshi Mudrabett <144533777+sakshimudrabett@users.noreply.github.com>
2024-04-23 05:49:24 +0000

    updated

--- 01_basics/memory.js
+++
@@ -1,8 +0,0 @@
-//Memory - two types:
-//Stack Memory (primitive) & Heap Mmemory(non primitive)
-
-let myytname="sakshim"
-let anname=myytname
-anname="chaiaurcode"
-console.log(myytname);//sakshim
-console.log(anname);//chaiaurcode
