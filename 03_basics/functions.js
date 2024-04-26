d666cd7291d354647d065ceab925d6f958266a72
Sakshi Mudrabett <144533777+sakshimudrabett@users.noreply.github.com>
2024-04-26 10:52:27 +0000

    functions

---
+++ 03_basics/functions.js
@@ -0,0 +1,16 @@
+function weekdays(){
+    console.log("monday");
+    console.log("tuesday");
+    console.log("wednesday");
+    console.log("thrusday");
+    console.log("friday");
+}
+//weekdays()
+
+function addtwonumbers(num1,num2){
+    return num1+num2
+}
+const result=addtwonumbers(4,5)//9
+const res=addtwonumbers(4,"5")//45
+//console.log(result);
+//console.log(res);
