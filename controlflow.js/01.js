3604b4b47e6c821a88d47d707c15e839ac44be65
Sakshi Mudrabett <144533777+sakshimudrabett@users.noreply.github.com>
2024-04-29 15:31:25 +0000

    controlflow-1

---
+++ controlflow.js/01.js
@@ -0,0 +1,36 @@
+//if
+
+/*if(condition){
+}
+//the condition must be true to enter  into if control flow
+*/
+/*
+let user1=5
+let user2=7
+if(user1<user2){
+    console.log("executed");
+}
+else{
+    console.log("check again!");
+}
+console.log("done with execution");*/
+
+//shorthand technique
+
+let balance=1000
+
+
+//if(balance>500) console.log("tested");//you can write in one statement without curly brackets , but it should end with semicolon
+
+
+//nesting
+/*if (balance<700) {
+    console.log("tested once");
+}else if(balance>500){
+    console.log("tested twice");//tested thrice
+}
+else{
+    console.log("tested thrice");
+}
+*/
+
