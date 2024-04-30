1be10d963ccbcbb1836c8539cd40a1efcf2a85de
Sakshi Mudrabett <144533777+sakshimudrabett@users.noreply.github.com>
2024-04-30 12:58:36 +0000

    while and do while

---
+++ loops.js/whiledowhile.js
@@ -0,0 +1,22 @@
+//-----while-----------
+/*
+let i=0
+while(i<4) {
+    console.log(i);
+    i++;
+}*/
+
+/*
+let myarray=[1,2,3,4]
+let arr=0
+while(arr<myarray.length){
+    console.log(arr);
+    arr++;
+}*/
+
+//----do-while-----------
+let score=1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=5);
